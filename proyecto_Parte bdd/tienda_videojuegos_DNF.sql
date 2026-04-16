7adf4aef619ed4223d296bff5a56cfaefdfe9205
Diego Navarro <diegonavarrofernandez07@gmail.com>
2026-04-16 12:14:01 +0200

    Add files via upload

--- proyecto_Parte bdd/tienda_videojuegos_DNF.sql
+++ proyecto_Parte bdd/tienda_videojuegos_DNF.sql
@@ -12,55 +12,55 @@ USE tiendaVideojuegos;
 
 CREATE TABLE tienda (
 	id int PRIMARY key Auto_increment,
-	nombre_tienda varchar(30) NOT NULL,
+	nombre_tienda VARCHAR(30) NOT NULL,
 	telefono int(9) not NULL,
-	stock int,
-	ubicacion varchar(40)
+	stock INT,
+	ubicacion VARCHAR(40)
 	
 	
 );
 
 CREATE TABLE empleado (
 	
-	dni char(9) PRIMARY KEY,
+	dni CHAR(9) PRIMARY KEY,
 	nombre VARCHAR(30) not NULL,
 	apellidos VARCHAR(50) not NULL,
-	telefono int(9),
-	id_tienda int ,
-	dni_supervisor char(9),
+	telefono INT(9),
+	id_tienda INT ,
+	dni_supervisor CHAR(9),
 	
-	CONSTRAINT fk_IDentificacion_tienda FOREIGN KEY(id_tienda) REFERENCES tienda (id)
+	CONSTRAINT FK_IDENTIFICACION_TIENDA FOREIGN KEY (id_tienda) REFERENCES tienda (id)
 	
 	
 );
 
 CREATE table dependiente (
 	
-	dni_dependiente char(9) PRIMARY KEY,
+	dni_dependiente CHAR(9) PRIMARY KEY,
 	
-	CONSTRAINT fk_dni_dependiente FOREIGN key (dni_dependiente) REFERENCES empleado (dni)
+	CONSTRAINT fk_dni_dependiente FOREIGN KEY (dni_dependiente) REFERENCES empleado (dni)
 
 
 );
 
 CREATE table reponedor (
 	
-	dni_reponedor char(9) PRIMARY KEY,
+	dni_reponedor CHAR(9) PRIMARY KEY,
 	
-	CONSTRAINT fk_dni_reponedor FOREIGN key (dni_reponedor) REFERENCES empleado (dni)
+	CONSTRAINT fk_dni_reponedor FOREIGN KEY (dni_reponedor) REFERENCES empleado (dni)
 
 
 );
 
 CREATE table cliente (
 
-	dni char(9) PRIMARY KEY,
+	dni CHAR(9) PRIMARY KEY,
 	nombre VARCHAR(30) not NULL,
 	apellidos VARCHAR(50) not NULL,
-	telefono int(9),
-	correo varchar(50) UNIQUE,
-	dni_dependiente char(9),
-	CONSTRAINT fk_dni_dependiente_atiende FOREIGN key (dni_dependiente) REFERENCES dependiente (dni_dependiente)
+	telefono INT(9),
+	correo VARCHAR(50) UNIQUE,
+	dni_dependiente CHAR(9),
+	CONSTRAINT fk_dni_dependiente_atiende FOREIGN KEY (dni_dependiente) REFERENCES dependiente (dni_dependiente)
 
 
 
@@ -68,24 +68,24 @@ CREATE table cliente (
 
 create table proveedor (
 
-	id int PRIMARY key Auto_increment,
-	nombre_tienda varchar(30) NOT NULL,
-	telefono int(9) not NULL,
-	ubicacion varchar(40)
+	id INT PRIMARY KEY Auto_increment,
+	nombre_tienda VARCHAR(30) NOT NULL,
+	telefono INT(9) not NULL,
+	ubicacion VARCHAR(40)
 
 
 );
 
 create table videojuego (
 
-	numero_serie char(9)  PRIMARY key,
-	nombre varchar(30) not null UNIQUE,
-	genero varchar(20),
+	numero_serie CHAR(9)  PRIMARY KEY,
+	nombre VARCHAR(30) not null UNIQUE,
+	genero VARCHAR(20),
 	id_proveedor int,
 	precio DECIMAL(4,2),
 	
 	
-	CONSTRAINT fk_id_proveedor FOREIGN key (id_proveedor) REFERENCES proveedor (id)
+	CONSTRAINT fk_id_proveedor FOREIGN KEY (id_proveedor) REFERENCES proveedor (id)
 
 
 
@@ -94,43 +94,43 @@ create table videojuego (
 
 create table ficha (
 
-	id_registro int Primary Key Auto_increment,
-	fechs_entrada date not null,
-	fecha_Salida date not  null,
-	dni_empleado char(9),
+	id_registro INT Primary Key Auto_increment,
+	fechs_entrada DATE not null,
+	fecha_Salida DATE not  null,
+	dni_empleado CHAR(9),
 	
 	
-	CONSTRAINT fk_dni_empleado FOREIGN key (dni_empleado) REFERENCES empleado (dni)
+	CONSTRAINT fk_dni_empleado FOREIGN KEY (dni_empleado) REFERENCES empleado (dni)
 
 
 );
 
 CREATE TABLE TIENDA_VIDEOJUEGO (
-    id_tienda int NOT NULL,
-    num_serie char(9) NOT NULL,    
-	precio_juego DECIMAL(4,2),
+    id_tienda INT,
+    num_serie CHAR(9),    
     PRIMARY KEY (id_tienda, num_serie),
- 
-	CONSTRAINT fk_id_tienda FOREIGN key (id_tienda) REFERENCES tienda (id),
-	CONSTRAINT fk_num_serie FOREIGN key (num_serie) REFERENCES videojuego (numero_serie),
-	CONSTRAINT fk_precio_juego FOREIGN key (precio_juego) REFERENCES videojuego (precio)
+	
+	CONSTRAINT FK_ID_TIENDA FOREIGN KEY (id_tienda) REFERENCES tienda (id),
+	CONSTRAINT FK_NUM_TIENDA FOREIGN KEY (num_serie) REFERENCES videojuego (numero_serie)
+	
+	
 
 );
 
 
 create table cliente_videojuego (
 
-	dni_cliente   char(9) ,
-    numero_serie  CHAR(9) ,
-	nombre_juego varchar(30),
-	precio_juego DECIMAL(4,2),
+	dni_cliente CHAR(9) ,
+    numero_serie CHAR(9) ,
     
     PRIMARY KEY (dni_cliente, numero_serie),
 	
-	CONSTRAINT fk_numero_serie FOREIGN key (num_serie) REFERENCES videojuego (numero_serie),
-    CONSTRAINT fk_dni_ciente FOREIGN key (dni_cliente) REFERENCES cliente (dni),
-	CONSTRAINT fk_precio_del_juego FOREIGN key (precio_juego) REFERENCES videojuego (precio),
-	CONSTRAINT fk_nombre_juego FOREIGN key (nombre_juego) REFERENCES videojuego (nombre)
+	CONSTRAINT FK_DNI_CLIENTE FOREIGN KEY (dni_cliente) REFERENCES cliente (dni),
+	CONSTRAINT FK_NUMERO_SERIE FOREIGN KEY (numero_serie) REFERENCES videojuego (numero_serie)
+	
+	
+  
+
 
 );
 
